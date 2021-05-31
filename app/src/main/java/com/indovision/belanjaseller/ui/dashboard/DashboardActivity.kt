2d1c0f58c001a51e4037c9137e8c39f654290958
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-31 14:18:16 +0700

    create dashboard activity

---
+++ app/src/main/java/com/indovision/belanjaseller/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,15 @@
+package com.indovision.belanjaseller.ui.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.indovision.belanjaseller.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+}
