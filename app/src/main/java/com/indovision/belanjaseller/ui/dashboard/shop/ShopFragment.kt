e963f52495fb6bde658563b16a133f8fde51a68a
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-06-01 10:57:55 +0700

    create shop fragment

---
+++ app/src/main/java/com/indovision/belanjaseller/ui/dashboard/shop/ShopFragment.kt
@@ -0,0 +1,27 @@
+package com.indovision.belanjaseller.ui.dashboard.shop
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.indovision.belanjaseller.databinding.FragmentShopBinding
+
+class ShopFragment : Fragment() {
+    private var _binding: FragmentShopBinding? = null
+    private val binding get() = _binding as FragmentShopBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
